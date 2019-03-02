0d367faaaca86ebd20c5f3b957044cf9db388824
Corwin Marsh <haphazardhero@gmail.com>
2019-03-02 15:20:33 -0800

    Initial Commit

---
+++ Hood-Ranker-iOSTests/Hood_Ranker_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hood_Ranker_iOSTests.swift
+//  Hood-Ranker-iOSTests
+//
+//  Created by Corwin Marsh on 3/2/19.
+//  Copyright © 2019 Open Data Hack the Planet. All rights reserved.
+//
+
+import XCTest
+@testable import Hood_Ranker_iOS
+
+class Hood_Ranker_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
