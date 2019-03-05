967d126779e64172cf5e67ea4456a65dcdfb7bc0
Christopher Moore <christopherkmoore1@gmail.com>
2019-03-04 18:29:35 -0800

    babys first layer

--- Hood-Ranker-iOS/MapViewController.swift
+++ Hood-Ranker-iOS/MapViewController.swift
@@ -18,8 +18,16 @@ class MapViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
 
         mapView.map = AGSMap(basemapType: .navigationVector, latitude: 47.6062, longitude: -122.3321, levelOfDetail: 10)
+        
+        addLayers()
     }
 
+    func addLayers() {
+        let publicHealthURL = URL(string: "https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/pubsafe__restaurant_inspections_point/MapServer/857")
+        let featureTable = AGSServiceFeatureTable(url: publicHealthURL!)
+        let layer = AGSFeatureLayer(featureTable: featureTable)
+        mapView.map?.operationalLayers.add(layer)
+    }
 
 }
 
